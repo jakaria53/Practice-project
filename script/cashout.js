670816ff724f1be9965275dedfd025f9c87e163f
jakaria53 <jakariachowdhury53@gmail.com>
2026-03-26 22:41:30 +0600

    update4

--- script/cashout.js
+++ script/cashout.js
@@ -1,6 +1,41 @@
 document.getElementById("cashout-btn").addEventListener("click", function() {
 
-    
+    const cashOutNumberInput = document.getElementById("cashout-number");
+     const cashOutNumber = cashOutNumberInput.value;
+     console.log(cashOutNumber);
+     if(cashOutNumber.length !== 11 ){
+        alert("Invalid phone number. Please enter an 11-digit number.");
+        return;
+     }
+
+    const cashOutAmountInput = document.getElementById("cashout-amount");
+    const cashOutAmount = cashOutAmountInput.value;
+    console.log(cashOutAmount);
+
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+    const newBalance = Number(balance) - Number(cashOutAmount);
+    console.log(newBalance);
+    if (newBalance < 0) {
+        alert("Insufficient balance. Please try again.");
+        return;
+    }
+       console.log("New Balance:",newBalance);
+
+
+       const cashOutPinInput = document.getElementById("cashout-pin");
+       const Pin = cashOutPinInput.value;
+       if (Pin === "1234") {
+
+        alert("Cash out successful! Your new balance is: " + newBalance);
+        balanceElement.innerText = newBalance;
+       }
+       else {
+        alert("Invalid PIN. Please try again.");
+        return;
+       }
 
 
 });
